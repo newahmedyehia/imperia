c879159787b1e3e0ec33b035c7d310baad0863e4
newahmedyehia <124242960+newahmedyehia@users.noreply.github.com>
2023-03-19 21:58:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,5 @@
-from transformers import pipeline
-from PIL import Image
 
-import pytesseract
 import streamlit as st
 st.title("hello")
 st.header("Header")
 
-nlp = pipeline(
-    "document-question-answering",
-    model="impira/layoutlm-document-qa",
-)
-
-nlp(
-    "https://templates.invoicehome.com/invoice-template-us-neat-750px.png",
-    "What is the invoice number?"
-)
-# {'score': 0.9943977, 'answer': 'us-001', 'start': 15, 'end': 15}
-
-nlp(
-    "https://miro.medium.com/max/787/1*iECQRIiOGTmEFLdWkVIH2g.jpeg",
-    "What is the purchase amount?"
-)
-# {'score': 0.9912159, 'answer': '$1,000,000,000', 'start': 97, 'end': 97}
-
-nlp(
-    "https://www.accountingcoach.com/wp-content/uploads/2013/10/income-statement-example@2x.png",
-    "What are the 2020 net sales?"
-)
-# {'score': 0.59147286, 'answer': '$ 3,750', 'start': 19, 'end': 20}
